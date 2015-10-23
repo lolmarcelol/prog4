886ceec7d4d4dbee04dc771b4579aade88399ddf
marcelo.mikosz <marcelo.mikosz@LCET0917.cwbdis.pucpr.br>
2015-10-23 19:47:05 -0200

    jpa conti

--- projetoLoja/src/java/br/pucpr/prog4/lojaoldschool/models/Automovel.java
+++ projetoLoja/src/java/br/pucpr/prog4/lojaoldschool/models/Automovel.java
@@ -1,9 +1,11 @@
 package br.pucpr.prog4.lojaoldschool.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 
@@ -13,8 +15,21 @@ public class Automovel {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     
     private long id;
+    
+    @Column(nullable=false,length=7)
     private String placa;
     private int anoFabricacao;
+    
+    @ManyToOne
+    private Modelo modelo;
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(Modelo modelo) {
+        this.modelo = modelo;
+    }
 
     public long getId() {
         return id;
--- projetoLoja/src/java/br/pucpr/prog4/lojaoldschool/models/Modelo.java
+++ projetoLoja/src/java/br/pucpr/prog4/lojaoldschool/models/Modelo.java
@@ -1,7 +1,18 @@
 
 package br.pucpr.prog4.lojaoldschool.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Modelo {
+      
+    @Id
+    @GeneratedValue()
     
+    private long id;
+    private String nome;
+    private int anoCriacao;
     
 }
